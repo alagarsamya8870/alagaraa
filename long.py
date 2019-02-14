f86f17def4ba0bc852394365ffd7fa24c9c89f4b
alagarsamya8870 <42169898+alagarsamya8870@users.noreply.github.com>
2019-02-14 10:30:10 +0530

    Update long.py

--- long.py
+++ long.py
@@ -5,9 +5,9 @@ def largest(arr,n):
             max = arr[i] 
     return max
 alist=[]
-n=int(input("enter the numbers"))
+n=int(input())
 for i in range (0,n,1):
-    arr=int(input("enter the arr num"))
+    arr=int(input())
     alist.append(arr) 
 Ans=largest(alist,n) 
-print ("Largest in given array is",Ans)
+print (Ans)
